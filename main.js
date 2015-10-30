6a9d84f758a2d2bd745fdfe707fe63bc0687faf1
flyingrub <flyinggrub@gmail.com>
2015-10-30 01:01:46 +0100

    Fix: touch mobile

--- main.js
+++ main.js
@@ -135,9 +135,22 @@ window.ontouchmove = function(e) {
 }
 
 window.ontouchstart = function(e) {
-    if (e.target.nodeName !== 'INPUT') {
-        e.preventDefault();
-    }	
+	evt.preventDefault();
+	if (evt.touches.length > 1 || (evt.type == "touchend" && evt.touches.length > 0))
+		return;
+
+	var newEvt = document.createEvent("MouseEvents");
+	var type = null;
+	var touch = null;
+	switch (event.type) {
+		case "touchstart": type = "mousedown"; touch = event.changedTouches[0];
+		case "touchmove":  type = "mousemove"; touch = event.changedTouches[0];
+		case "touchend":   type = "mouseup"; touch = event.changedTouches[0];
+	}
+	newEvt.initMouseEvent(type, true, true, event.originalTarget.ownerDocument.defaultView, 0,
+	touch.screenX, touch.screenY, touch.clientX, touch.clientY,
+	evt.ctrlKey, evt.altKey, evt.shirtKey, evt.metaKey, 0, null);
+	event.originalTarget.dispatchEvent(newEvt);
 }
 
 // Particle
